1c853b180213cb94ac14d6f7e9ceff5c435036ac
Le96 <blitz1216@gmail.com>
2022-02-28 19:29:15 +0900

    Add ErrorMessage schema

--- api/app/schemas.py
+++ api/app/schemas.py
@@ -4,6 +4,10 @@ from typing import Optional
 from pydantic import BaseModel
 
 
+class ErrorMessage(BaseModel):
+    detail: str
+
+
 class ORMModel(BaseModel):
     class Config:
         orm_mode = True
