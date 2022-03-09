30c648cd2ef29b780d94f41f6ffdceffa3a6eb54
Le96 <blitz1216@gmail.com>
2022-03-10 01:08:11 +0900

    Rename leaderboard variable

--- web/src/components/LeaderboardGraph.jsx
+++ web/src/components/LeaderboardGraph.jsx
@@ -8,19 +8,19 @@ import { Paper } from '@mui/material';
 import { getLeaderboard } from './api';
 
 export default function LeaderboardGraph() {
-  const [leaderboardData, setLeaderboardData] = useState(false);
+  const [leaderboard, setLeaderboard] = useState(false);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     if (loaded) return;
     setLoaded(true);
-    getLeaderboard().then((response) => setLeaderboardData(response.data));
+    getLeaderboard().then((response) => setLeaderboard(response.data));
   }, [loaded]);
 
   return (
     <Paper variant="outlined" sx={{ flexGrow: 1 }}>
-      {leaderboardData && (
-        <Chart data={leaderboardData} rotated>
+      {leaderboard && (
+        <Chart data={leaderboard} rotated>
           <ArgumentAxis />
           <ValueAxis />
           <BarSeries argumentField="key" color="green" valueField="value" />
